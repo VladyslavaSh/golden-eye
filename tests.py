fd64f4086ed8d2bdbe905036030ed626d8c3b7fe
vlshek <vlshek@ttu.ee>
2022-08-01 12:09:33 +0000

    Remove test for test_api.py

--- tests.py
+++ tests.py
@@ -3,9 +3,9 @@ import json
 from unittest.mock import patch
 
 import models
-import api.test_api as test_api
 import api.privat_api as privat_api
 import api.monobank_api as monobank_api
+import api
 
 
 def get_privat_response(*args, **kwds):
@@ -24,17 +24,6 @@ def get_privat_response(*args, **kwds):
 class Test(unittest.TestCase):
     def setUp(self):
         models.init_db()
-
-    def test_main(self):
-        xrate = models.XRate.get(id=1)
-        updated_before = xrate.updated
-        self.assertEqual(xrate.rate, 1.0)
-        test_api.update_xrates(840, 980)
-        xrate = models.XRate.get(id=1)
-        updated_after = xrate.updated
-
-        self.assertEqual(xrate.rate, 1.01)
-        self.assertGreater(updated_after, updated_before)
 
     def test_privat(self):
         xrate = models.XRate.get(id=1)
