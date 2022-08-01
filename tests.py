24cb5ce30bb87c81055d032de2c9739865b5964c
vlshek <vlshek@ttu.ee>
2022-08-01 11:55:18 +0000

    Expand test_privat and test_monobank + add test_privat_mock

--- tests.py
+++ tests.py
@@ -1,9 +1,24 @@
 import unittest
+import json
+from unittest.mock import patch
 
-import api.test_api
 import models
-import api.privat_api
-import api.monobank_api
+import api.test_api as test_api
+import api.privat_api as privat_api
+import api.monobank_api as monobank_api
+
+
+def get_privat_response(*args, **kwds):
+    print("get_privat_response")
+
+    class Response:
+        def __init__(self, response):
+            self.text = json.dumps(response)
+
+        def json(self):
+            return json.loads(self.text)
+
+    return Response([{"ccy": "USD", "base_ccy": "UAH", "sale": "30.0"}])
 
 
 class Test(unittest.TestCase):
@@ -14,7 +29,7 @@ class Test(unittest.TestCase):
         xrate = models.XRate.get(id=1)
         updated_before = xrate.updated
         self.assertEqual(xrate.rate, 1.0)
-        api.test_api.update_xrates(840, 980)
+        test_api.update_xrates(840, 980)
         xrate = models.XRate.get(id=1)
         updated_after = xrate.updated
 
@@ -25,23 +40,64 @@ class Test(unittest.TestCase):
         xrate = models.XRate.get(id=1)
         updated_before = xrate.updated
         self.assertEqual(xrate.rate, 1.0)
-        api.privat_api.update_xrates(840, 980)
+
+        privat_api.Api().update_rate(840, 980)
+
         xrate = models.XRate.get(id=1)
         updated_after = xrate.updated
 
         self.assertGreater(xrate.rate, 25)
         self.assertGreater(updated_after, updated_before)
 
+        # check the record in ApiLog
+        api_log = models.ApiLog.select().order_by(models.ApiLog.created.desc()).first()
+
+        self.assertIsNotNone(api_log)  # check existence
+        self.assertEqual(api_log.request_url, "https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11")
+        self.assertIsNotNone(api_log.response_text)
+
+        self.assertIn('{"ccy":"USD","base_ccy":"UAH",', api_log.response_text)
+
     def test_monobank(self):
         xrate = models.XRate.get(from_currency=978, to_currency=980)
         updated_before = xrate.updated
         self.assertEqual(xrate.rate, 1.0)
-        api.monobank_api.update_xrates(978, 980)
+
+        monobank_api.Api().update_rate(978, 980)
+
         xrate = models.XRate.get(from_currency=978, to_currency=980)
         updated_after = xrate.updated
 
         self.assertGreater(xrate.rate, 25)
         self.assertGreater(updated_after, updated_before)
+
+        self.assertIsNotNone(api_log)
+        self.assertEqual(api_log.request_url, "https://api.monobank.ua/bank/currency")
+        self.assertIsNotNone(api_log.response_text)
+
+        self.assertIn('{"currencyCodeA":978,"currencyCodeB":980,', api_log.response_text)
+    
+    @patch('api._Api._send', new=get_privat_response)
+    def test_privat_mock(self):
+        xrate = models.XRate.get(id=1)
+        updated_before = xrate.updated
+        self.assertEqual(xrate.rate, 1.0)
+
+        privat_api.Api().update_rate(840, 980)
+
+        xrate = models.XRate.get(id=1)
+        updated_after = xrate.updated
+
+        self.assertEqual(xrate.rate, 30)
+        self.assertGreater(updated_after, updated_before)
+
+        api_log = models.ApiLog.select().order_by(models.ApiLog.created.desc()).first()
+
+        self.assertIsNotNone(api_log)
+        self.assertEqual(api_log.request_url, "https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11")
+        self.assertIsNotNone(api_log.response_text)
+
+        self.assertEqual('[{"ccy": "USD", "base_ccy": "UAH", "sale": "30.0"}]', api_log.response_text)
 
 
 if __name__ == '__main__':
