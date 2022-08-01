b76da84df4fbbd8de68e328b6d4685f56ad9c5dc
vlshek <vlshek@ttu.ee>
2022-08-01 11:04:50 +0000

    Add tables ApiLog and ErrorLog to models.py

--- models.py
+++ models.py
@@ -1,13 +1,17 @@
-from peewee import SqliteDatabase, Model, IntegerField, DoubleField, DateTimeField, datetime as peewee_datetime
+from peewee import (SqliteDatabase, Model, IntegerField, DoubleField, DateTimeField, datetime as peewee_datetime, CharField, TextField)
 
 from config import DB_NAME
 
 db = SqliteDatabase(DB_NAME)
 
 
-class XRate(Model):
+class _Model(Model):
     class Meta:
         database = db
+
+
+class XRate(Model):
+    class Meta:
         db_table = "xrates"
         indexes = (
             (("from_currency", "to_currency"), True),
@@ -22,9 +26,44 @@ class XRate(Model):
         return "XRate(%s=>%s): %s" % (self.from_currency, self.to_currency, self.rate)
 
 
+# logging of request-response info into the DB
+class ApiLog(_Model):
+    class Meta:
+        db_table = "api_logs"
+
+    request_url = CharField()
+    request_data = TextField(null=True)
+    request_method = CharField(max_length=100)
+    request_headers = TextField(null=True)
+    response_text = TextField(null=True)
+    # with a creation of the table the index will be created
+    created = DateTimeField(index=True, default=peewee_datetime.datetime.now)
+    finished = DateTimeField()
+    error = TextField(null=True)
+
+
+# logging of error info into the DB
+class ErrorLog(_Model):
+    class Meta:
+        db_table = "error_logs"
+
+    request_data = TextField(null=True)
+    request_url = TextField()
+    request_method = CharField(max_length=100)
+    error = TextField()
+    traceback = TextField(null=True)
+    created = DateTimeField(default=peewee_datetime.datetime.now, index=True)
+
+
 def init_db():
-    db.drop_tables(XRate)
+    XRate.drop_table()
     XRate.create_table()
     XRate.create(from_currency=840, to_currency=980, rate=1)
     XRate.create(from_currency=978, to_currency=980, rate=1)
+
+    for m in (ApiLog, ErrorLog):
+        m.drop_table()
+        m.create_table()
+
+
     print("db created!")
