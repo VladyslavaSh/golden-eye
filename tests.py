78c3da10c0bde219a272f7e9b966a6d5b4a7a48d
vlshek <vlshek@ttu.ee>
2022-08-03 13:58:46 +0000

    Add tests to the API methods

--- tests.py
+++ tests.py
@@ -164,6 +164,32 @@ class Test(unittest.TestCase):
         self.assertEqual(api_log.request_url, "https://rest.coinapi.io/v1/exchangerate/BTC/UAH")
         self.assertIsNotNone(api_log.response_text)
 
+    def test_xml_api(self):
+        r = requests.get("http://localhost:5000/api/xrates/xml")
+        self.assertIn("<xrates>", r.text)
+        xml_rates = xmltodict.parse(r.text)
+        self.assertIn("xrates", xml_rates)  # check the key "xrates"
+        self.assertIsInstance(xml_rates["xrates"]["xrate"], list)
+        self.assertEqual(len(xml_rates["xrates"]["xrate"]), 5)
+
+    def test_json_api(self):
+        r = requests.get("http://localhost:5000/api/xrates/json")
+        json_rates = r.json()
+        self.assertIsInstance(json_rates, list)
+        self.assertEqual(len(json_rates), 5)
+        for rate in json_rates:
+            self.assertIn("from", rate)
+            self.assertIn("to", rate)
+            self.assertIn("rate", rate)
+
+    def test_json_api_uah(self):
+        r = requests.get("http://localhost:5000/api/xrates/json?to_currency=980")
+        json_rates = r.json()
+        self.assertIsInstance(json_rates, list)
+        self.assertEqual(len(json_rates), 3)
+        for rate in json_rates:
+            self.assertEqual(rate["to"], 980)
+
 
 if __name__ == '__main__':
     unittest.main()
